c82362a22bf6af53105dd55ad38b12ced5513f7a
Nguyễn Văn Quyết <121377981+Paul-Greyrat@users.noreply.github.com>
2024-06-06 14:28:03 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -6,7 +6,7 @@ const loginBtn = document.getElementById('login');
 
 registerBtn.addEventListener('click', () =>{ 
   container.classList.add("active");
-});
+}); 
 loginBtn.addEventListener('click', () =>{ 
   container.classList.remove("active");
 }); 
